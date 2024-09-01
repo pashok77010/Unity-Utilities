fffc3a519d53a80f87f06f0ee8b028ebc058dfe0
pashok77010 <42540664+pashok77010@users.noreply.github.com>
2024-09-02 03:09:56 +0400

    Update AppVersion.cs

--- AppVersion.cs
+++ AppVersion.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class AppVersion : MonoBehaviour
 {
     public Text text;
+
+    void OnValidate()
+    {
+        if(text == null) text = GetComponent<Text>();
+    }
     
     void Start()
     {
